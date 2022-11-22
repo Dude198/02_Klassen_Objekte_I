612c6519245aab1613d4c1408107cf434a121790
Marcel <marcel.eggert1@outlook,de>
2022-11-22 12:44:25 +0100

    WHAT

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,14 +3,32 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+    
+    Cat cat = new Cat();
+    output("Blick von aussen: " + cat);
+
+    cat.tellYourAdress();
+
+     output("--------------");
+
+     Cat cat1 = new Cat();
+     output("Blick von aussen: " + cat1);
+ 
+     cat1.tellYourAdress();
+
+      App.tellYourAdress();
+
+
+ 
     }
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
 
-
+    public static void tellYourAdress() {
+        System.out.println("Hallo!");
+    }
 
 }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class Cat {
+
+public void tellYourAdress(){
+    // System.out.println("Blick von innen: " + this);
+    App.output("Blick von innen: " + this);    
+}
+
+
+
+    
+}
